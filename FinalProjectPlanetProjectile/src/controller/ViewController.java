5efb3c7406a51745af94b4aee1257685e723abd7
lynasellami <2488446@edu.vaniercollege.qc.ca>
2025-11-25 23:51:11 -0500

    Add Planet model and planets array

--- FinalProjectPlanetProjectile/src/controller/ViewController.java
+++ FinalProjectPlanetProjectile/src/controller/ViewController.java
@@ -1,123 +1,51 @@
 package controller;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import Model.Planet;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
-/**
- * FXML Controller class
- *
- * @author louay
- */
-public class ViewController implements Initializable {
-
-    
-
+public class ViewController {
 
     @FXML
-    private AnchorPane WelcomeMenuPane;
+    private Pane WelcomeMenuPane;
+
     @FXML
-    private ImageView BackgroundMenu;
-    @FXML
-    private Label welcomeLabel;
-    @FXML
-    private Label clickLabel;
-    @FXML
-    private ImageView venusMenu;
-    @FXML
-    private ImageView mercuryMenu;
-    @FXML
-    private ImageView earthMenu;
-    @FXML
-    private ImageView marsMenu;
-    @FXML
-    private ImageView jupiterMenu;
-    @FXML
-    private ImageView saturnMenu;
-    @FXML
-    private ImageView uranusMenu;
-    @FXML
-    private ImageView neptuneMenu;
-    @FXML
-    private ImageView BackgroudSecond;
+    private Pane BackgroundSecond;
+
     @FXML
     private Label planetName;
+
     @FXML
     private ImageView planetGround;
-    @FXML
-    private ImageView start;
-    @FXML
-    private ImageView flower;
-    @FXML
-    private ImageView soccer;
-    @FXML
-    private ImageView apple;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO  //////
-    }    
-
-    
-    @FXML
-    private void venusClicked(MouseEvent event) {
-    }
+    // STEP 1: array that will hold all the planets
+    private Planet[] planets;
 
     @FXML
-    private void mercuryClicked(MouseEvent event) {
+    public void initialize() {
+        // STEP 1: only set up the data – UI logic comes later
+        planets = new Planet[] {
+                new Planet(1, "Mercury", 3.70, "/View/planet1.png"),
+                new Planet(2, "Venus",   8.87, "/View/planet2.png"),
+                new Planet(3, "Earth",   9.81, "/View/planet3.png"),
+                new Planet(4, "Mars",    3.71, "/View/planet4.png"),
+                new Planet(5, "Jupiter", 24.79, "/View/planet5.png"),
+                new Planet(6, "Saturn",  10.44, "/View/planet6.png"),
+                new Planet(7, "Uranus",   8.69, "/View/planet7.png"),
+                new Planet(8, "Neptune", 11.15, "/View/planet8.png")
+        };
     }
 
-    @FXML
-    private void earthClicked(MouseEvent event) {
+    // temporary version – we will improve this in the next commit
+    private void openPlanetScreen(int id) {
+        // just switch screens for now
+        WelcomeMenuPane.setVisible(false);
+        BackgroundSecond.setVisible(true);
     }
 
-    @FXML
-    private void marsClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void jupiterClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void saturnClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void uranusClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void neptuneClicked(MouseEvent event) {
-    }
-  
-    @FXML
-    private void startClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void flowerClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void soccerClicked(MouseEvent event) {
-    }
-
-    @FXML
-    private void appleClicked(MouseEvent event) {
-    }
-    
+    // click methods come in a later commit
 }
